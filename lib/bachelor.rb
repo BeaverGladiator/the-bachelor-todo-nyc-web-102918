c6623fbbc4cb7d31f61a0484fae2901aefa4e454
Matthew Migliore <mmigliore311@gmail.com>
2018-10-18 17:56:09 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -55,12 +55,12 @@ def get_occupation(data, hometown)
     data.each do |contestant|
       if contestant["hometown"] == hometown
         occupation = contestant["occupation"]
+        return occupation
       end
     end
    
   end 
-  
-  occupation
+
 end
 
 def get_average_age_for_season(data, season)
