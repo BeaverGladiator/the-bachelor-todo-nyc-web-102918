10863682d229295b3a886239dd2b84c4f664b056
Matthew Migliore <mmigliore311@gmail.com>
2018-10-18 17:47:24 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,13 +1,48 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
   # code here
+  winner = ""
+  data.each do |season_num, data|
+    if season_num == season
+      data.each do |contestant|
+        if contestant["status"] == "Winner"
+          winner = contestant["name"]
+        end
+      end
+    end 
+  end 
+
+  winner_names = winner.split(" ")
+  winner_names[0]
 end
 
 def get_contestant_name(data, occupation)
   # code here
+  name = ""
+  
+  data.each do |season, data|
+    data.each do |contestant|
+      if contestant["occupation"] == occupation
+         name = contestant["name"]
+      end
+    end
+  end 
+  
+  name
 end
 
 def count_contestants_by_hometown(data, hometown)
   # code here
+  counter = 0
+  
+  data.each do |season, data|
+    data.each do |contestant|
+      if contestant["occupation"] == occupation
+         name = contestant["name"]
+      end
+    end
+  end 
 end
 
 def get_occupation(data, hometown)
